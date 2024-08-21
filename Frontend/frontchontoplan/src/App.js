739baa9d395e3f93980c04d0ac59b34cd3121d3a
Sebastián Valverde <90531569+SebastianValverde911@users.noreply.github.com>
2024-08-20 20:42:40 -0500

    Se realiza validacion en los campos de email y contra (#2)
    Co-authored-by: SebastianValverde911 <juan_seb.valverde@uao.edu.co>

---
+++ Frontend/frontchontoplan/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './components/components-login/Login.jsx';
+
+
+function App() {
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
