1b63796e8a2acdffcd13ee29800df2381b6313a8
zarrok <camilo_alb.velasquez@uao.edu.co>
2024-08-21 23:13:20 -0500

    login and singup

---
+++ Backend/CTP_Backend/api/serializers.py
@@ -0,0 +1,37 @@
+from abc import ABC
+import os
+
+from rest_framework import serializers
+from django.core.exceptions import ObjectDoesNotExist
+
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'name' , 'password', 'role']
+
+    @staticmethod
+    def validate_email(value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email is already in use.")
+        return value
+
+class SignupSerializer(serializers.Serializer):
+    user_info = UserSerializer()
+
+    def create(self, validated_data):
+        # Obtener datos de los modelos
+        user_data = validated_data.pop('user_info')
+
+        # Crear Usuario
+        password = user_data.pop('password', None)
+
+        user = User(**user_data)
+        if password is not None:
+            user.set_password(password)
+        user.save()
+
+        return {'user': user}
