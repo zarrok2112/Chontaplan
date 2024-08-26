e44fea36aadeb365f9e5d5b2b2e70b7231faaf7a
zarrok <camilo_alb.velasquez@uao.edu.co>
2024-08-26 03:42:39 -0500

    implementacion de chatbot, eventos y correo

--- Backend/CTP_Backend/api/urls.py
+++ Backend/CTP_Backend/api/urls.py
@@ -6,5 +6,12 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair')
+    path('login/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('signup/confirmed/', views.ActivateAccountView.as_view(), name='signup-confirm'),
+    path('create-chat/', views.CreateChatView.as_view(), name='create-chat'),
+    path('send-message/', views.SendMessageView.as_view(), name='send-message'),
+    path('chatbot-webhook/', views.ChatbotWebhookView.as_view(), name='chatbot-webhook'),
+    path('get-messages/', views.GetMessagesView.as_view(), name='get-messages'),
+    path('events/', views.EventView.as_view(), name='event-list'),
+    path('events/<int:pk>/', views.EventView.as_view(), name='event-detail'),
 ]
