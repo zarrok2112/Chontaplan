ace041f2d20e6cae51138d5f73325adca255eca5
zarrok <camilo_alb.velasquez@uao.edu.co>
2024-08-27 20:15:25 -0500

    fix errors

--- Backend/CTP_Backend/api/admin.py
+++ Backend/CTP_Backend/api/admin.py
@@ -2,11 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User, EventType
+from .models import User
 
-class EventTypeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')  # Muestra el ID y el nombre en la lista de tipos de eventos
-    search_fields = ('name',)  # Añade la funcionalidad de búsqueda por nombre
 
 admin.site.register(User)
-admin.site.register(EventType, EventTypeAdmin)
