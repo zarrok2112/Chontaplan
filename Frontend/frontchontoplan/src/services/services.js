c536cd36c04bcef9d07d6f962bd58345b2d502cf
HANA0K4 <mateohanaoka@gmail.com>
2024-10-17 20:57:00 -0500

    Feat: Se agrego deleteEvent

--- Frontend/frontchontoplan/src/services/services.js
+++ Frontend/frontchontoplan/src/services/services.js
@@ -3,35 +3,33 @@ import axios from 'axios';
 class Services {
     signUp(data) {
         try {
-            return axios.post('http://localhost:8000/api/v1/user/signup/',data)
-        }
-        catch(e) {
-            console.log('error '+e);
+            return axios.post('http://localhost:8000/api/v1/user/signup/', data);
+        } catch (e) {
+            console.log('error ' + e);
         }
     }
-  
+
     async loginService(formdata) {
         try {
-            return await axios.post('http://localhost:8000/api/v1/user/login/',formdata,{
+            return await axios.post('http://localhost:8000/api/v1/user/login/', formdata, {
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
-                 },
+                },
             });
-        }
-        catch(e) {
-            console.log('error '+e);
+        } catch (e) {
+            console.log('error ' + e);
         }
     }
 
-    getActiveCount(token,user) {
-        return axios.get(`http://localhost:8000/api/v1/user/signup/confirmed?token=${token}&user_id=${user}`)
+    getActiveCount(token, user) {
+        return axios.get(`http://localhost:8000/api/v1/user/signup/confirmed?token=${token}&user_id=${user}`);
     }
 
-    registerEvent(token,data) {
-        return axios.post('http://localhost:8000/api/v1/events/',data,{
+    registerEvent(token, data) {
+        return axios.post('http://localhost:8000/api/v1/events/', data, {
             headers: {
                 Authorization: `Bearer ${token}`,
-             },
+            },
         });
     }
 
@@ -44,10 +42,19 @@ class Services {
     }
 
     createChat(token) {
-        return axios.post('http://localhost:8000/api/v1/chat/create-chat/',undefined,{
+        return axios.post('http://localhost:8000/api/v1/chat/create-chat/', undefined, {
             headers: {
                 Authorization: `Bearer ${token}`,
-             }
+            },
+        });
+    }
+
+    // Agregamos el método deleteEvent
+    deleteEvent(token, eventId) {
+        return axios.delete(`http://localhost:8000/api/v1/events/${eventId}/`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
         });
     }
 }
